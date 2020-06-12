144372487db2a49fa09985907fdb7e122734d27a
wookkl <57362359+wookkl@users.noreply.github.com>
2020-06-12 10:10:19 +0900

    add hc_sr04 driver

---
+++ Achro-I.MX6Q/fpga_driver/hc_sr04/.fpga_hc-sr04_driver.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/root/work/achroimx6q/fpga_driver/hc_sr04/fpga_hc-sr04_driver.ko := arm-none-linux-gnueabi-ld -EL -r  -T /root/work/achroimx6q/kernel/scripts/module-common.lds --build-id  -o /root/work/achroimx6q/fpga_driver/hc_sr04/fpga_hc-sr04_driver.ko /root/work/achroimx6q/fpga_driver/hc_sr04/fpga_hc-sr04_driver.o /root/work/achroimx6q/fpga_driver/hc_sr04/fpga_hc-sr04_driver.mod.o
