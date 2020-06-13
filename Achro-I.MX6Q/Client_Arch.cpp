5d18ec8228e74687af3c3a104193575dd2defc2a
wookkl <57362359+wookkl@users.noreply.github.com>
2020-06-13 13:53:04 +0900

    add thread func

--- Achro-I.MX6Q/Client_Arch.cpp
+++ Achro-I.MX6Q/Client_Arch.cpp
@@ -7,17 +7,23 @@
 #include<iostream>
 #include<fstream>
 #include<errno.h>
-using namespace std;
+#include<pthread.h>
+
 #define BUFF_SIZE 1024              //def buffsize
 #define SERV_IP "172.20.10.2"  
 #define SERV_PORT 8000
+void* t_recv_from_server();
+
 
 int main(int argc, char **argv) {
 	struct sockaddr_in clientaddr;
 	int client_sockfd;
 	int client_len;
 	int rcv_byte;
-	int msg[BUFF_SIZE]
+	int msg[BUFF_SIZE];
+	int pthread_t p_thread;
+	int thread_id;
+	int status;
 	if ((client_sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {  // create server socket
 		perror("error : ");
 		return 1;
@@ -37,8 +43,26 @@ int main(int argc, char **argv) {
 	}
 	else printf("Client_connect() is OK...\n\n");
 
+	thread_id = pthread_create(&p_thread, NULL, t_recv_from_server);
 	rcv_byte = recv(client_sockfd, msg, sizeof(msg), 0);
 	printf("%s",msg);
+
+	pthread_cancel(p_thread);
+	pthread_join(p_thread, (void**)&status) ;
 	close(client_sockfd);
 	return 0;
+}
+
+void* t_recv_from_server(){
+	int rcv_byte;
+	while(1)
+	{
+		rcv_byte=recv(client_sockfd, msg, sizeof(msg), 0);
+		switch(msg){
+			case	:
+			case	:
+			default	:
+				break;
+		}
+	}
 }
